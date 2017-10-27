3271b06930d73deae05d5e7ce6b740f7d460805d
Christopher Jacobs <cjacobs@drobo.com>
2017-10-26 17:42:18 -0700

    Add starship test, plug in some .travis.yml values.

--- src/Layout.spec.js
+++ src/Layout.spec.js
@@ -445,6 +445,98 @@ describe('Layout', function () {
             )
         })
         it('traversal')
+
+        it('starship!', () => {
+            let ship = new Layout()
+
+            let cockpit = ship.addSection(0, 0, 0, 0, 'cockpit')
+            expect(ship.toString()).to.equal(
+                'c\n'
+            )
+
+            let walkway = cockpit.addBottom({
+                width: 1, height: 5, name: 'walkway'
+            })
+            expect(ship.toString()).to.equal(
+                'c\n' +
+                'w\n' +
+                'w\n' +
+                'w\n' +
+                'w\n' +
+                'w\n'
+            )
+
+            walkway.addLeft({
+                width: 1, height: 2, name: 'quarters'
+            })
+            walkway.addRight({
+                width: 1, height: 2, name: 'quarters'
+            })
+            expect(ship.toString()).to.equal(
+                ' c \n' +
+                'qwq\n' +
+                'qwq\n' +
+                ' w \n' +
+                ' w \n' +
+                ' w \n'
+            )
+
+            let enginel = walkway.addLeft({
+                width: 1, height: 3, shift: 4, name: 'engine'
+            })
+            let enginer = walkway.addRight({
+                width: 1, height: 3, shift: 4, name: 'engine'
+            })
+            walkway.addBottom({
+                width: 1, height: 2, name: 'cargo'
+            })
+            expect(ship.toString()).to.equal(
+                ' c \n' +
+                'qwq\n' +
+                'qwq\n' +
+                ' w \n' +
+                ' w \n' +
+                'ewe\n' +
+                'ece\n' +
+                'ece\n'
+            )
+
+            let generatorl = walkway.addLeft({
+                width: 2, height: 2, shift: 2, name: 'generator'
+            })
+            generatorl.addLeft({
+                width: 1, height: 2, shift: -1, name: 'laser'
+            })
+            generatorl.addLeft({
+                width: 1, height: 2, shift: 1, name: 'shield'
+            })
+            let generatorr = walkway.addRight({
+                width: 2, height: 2, shift: 2, name: 'generator'
+            })
+            generatorr.addRight({
+                width: 1, height: 2, shift: -1, name: 'laser'
+            })
+            generatorr.addRight({
+                width: 1, height: 2, shift: 1, name: 'shield'
+            })
+            enginel.addLeft({
+                width: 1, height: 2, name: 'shield'
+            })
+            enginer.addRight({
+                width: 1, height: 2, name: 'shield'
+            })
+            expect(ship.toString()).to.equal(
+                '   c   \n' +
+                '  qwq  \n' +
+                'l qwq l\n' +
+                'lggwggl\n' +
+                'sggwggs\n' +
+                'ssewess\n' +
+                ' seces \n' +
+                '  ece  \n'
+            )
+            expect(ship.size).to.equal(56)
+        })
     })
 
     coverage(this, new Layout())
